c617d151dd585ce3e4463dc4b777ac5b4537f228
Alex Gor <ifresh.wp@gmail.com>
2014-10-10 18:15:03 +0400

    task6: logic finished & output needed

--- tasks/task6/main.cpp
+++ tasks/task6/main.cpp
@@ -67,35 +67,11 @@ int main( int argc, char** argv )
     MPI_Comm cDepthComm;
     checkres( MPI_Cart_sub( cubeComm, remainDims, &cDepthComm ) );
 
-    remainDims[0] = 1;  remainDims[1] = 1; remainDims[2] = 0;
-    MPI_Comm aSideComm;
-    checkres( MPI_Cart_sub( cubeComm, remainDims, &aSideComm ) );
-
-    remainDims[0] = 0;  remainDims[1] = 1; remainDims[2] = 1;
-    MPI_Comm bSideComm;
-    checkres( MPI_Cart_sub( cubeComm, remainDims, &bSideComm ) );
-
-    remainDims[0] = 1; remainDims[1] = 0; remainDims[2] = 1;
-    MPI_Comm cSideComm;
-    checkres( MPI_Cart_sub( cubeComm, remainDims, &cSideComm ) );
-
-    int myCubeRank = 0;
-    checkres( MPI_Comm_rank( cubeComm, &myCubeRank ) );
-
-    int myADepthId = 0;
-    checkres( MPI_Comm_rank( aDepthComm, &myADepthId ) );
-
-    int myBDepthId = 0;
-    checkres( MPI_Comm_rank( bDepthComm, &myBDepthId ) );
-
     int myCDepthId = 0;
     checkres( MPI_Comm_rank( cDepthComm, &myCDepthId ) );
 
-    int myASideId = 0;
-    checkres( MPI_Comm_rank( aSideComm, &myASideId ) );
-
-    int myBSideId = 0;
-    checkres( MPI_Comm_rank( bSideComm, &myBSideId ) );
+    int myCubeRank = 0;
+    checkres( MPI_Comm_rank( cubeComm, &myCubeRank ) );
 
     int cubeCoords[3];
     checkres( MPI_Cart_coords( cubeComm, myCubeRank, 3, cubeCoords ) );
@@ -119,58 +95,74 @@ int main( int argc, char** argv )
         long aBlockW = aSrcMatHeader.width / yDim;
         aBlock.strongSubmatrix( tempLine, 0, aBlockW * cubeCoords[1], aBlockH, aBlockW );
 
+        long matrixesDims[2] = { aBlock.height(), aBlock.width() };
+        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
+        ABLOCK = MPI_Type_vector_wrapper( aBlock.height(), aBlock.width(), aBlock.dataWidth(), MPI_TYPE );
+        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
+    }
+    else
+    {
+        long matrixesDims[2];
+        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
+
+        ABLOCK = MPI_Type_vector_wrapper( matrixesDims[0], matrixesDims[1], matrixesDims[1], MPI_TYPE );
+
+        aBlock = matrix< MATRIX_TYPE >( matrixesDims[0], matrixesDims[1] );
+        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
+    }
+    
+    if ( cubeCoords[0] == 0 )
+    {
+        matrix< MATRIX_TYPE > tempLine;
+
         SHeader bSrcMatHeader;
         SHeader bLineHeader;
-        tempLine = deserializeLine< MATRIX_TYPE >( matBFile, cubeCoords[2], zDim, &bSrcMatHeader, &bLineHeader );
+        tempLine = deserializeLine< MATRIX_TYPE >( matBFile, cubeCoords[1], zDim, &bSrcMatHeader, &bLineHeader );
         long bBlockH = bSrcMatHeader.height / zDim;
         long bBlockW = bSrcMatHeader.width / yDim;
-        bBlock.strongSubmatrix( tempLine, 0, bBlockW * cubeCoords[1], bBlockH, bBlockW );
+        bBlock.strongSubmatrix( tempLine, 0, bBlockW * cubeCoords[2], bBlockH, bBlockW );
 
-        if ( aSrcMatHeader.width != bSrcMatHeader.height )
-            throw "Invalid matrices dimensions( non equal )";
-
-        long matrixesDims[4] = { aBlock.height(), aBlock.width(), bBlock.height(), bBlock.width() };
-        MPI_Bcast( matrixesDims, 4, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
-
-        ABLOCK = MPI_Type_vector_wrapper( aBlock.height(), aBlock.width(), aBlock.dataWidth(), MPI_TYPE );
+        long matrixesDims[2] = { bBlock.height(), bBlock.width() };
+        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
         BBLOCK = MPI_Type_vector_wrapper( bBlock.height(), bBlock.width(), bBlock.dataWidth(), MPI_TYPE );
-
-        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
         MPI_Bcast( bBlock.raw(), 1, BBLOCK, ROOT_ID, bDepthComm );
     }
     else
     {
-        long matrixesDims[4];
-        MPI_Bcast( matrixesDims, 4, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
+        long matrixesDims[2];
+        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
 
-        ABLOCK = MPI_Type_vector_wrapper( matrixesDims[0], matrixesDims[1], matrixesDims[1], MPI_TYPE );
-        BBLOCK = MPI_Type_vector_wrapper( matrixesDims[2], matrixesDims[3], matrixesDims[3], MPI_TYPE );
+        BBLOCK = MPI_Type_vector_wrapper( matrixesDims[0], matrixesDims[1], matrixesDims[1], MPI_TYPE );
 
-        aBlock = matrix< MATRIX_TYPE >( matrixesDims[0], matrixesDims[1] );
-        bBlock = matrix< MATRIX_TYPE >( matrixesDims[2], matrixesDims[3] );
-        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
+        bBlock = matrix< MATRIX_TYPE >( matrixesDims[0], matrixesDims[1] );
         MPI_Bcast( bBlock.raw(), 1, BBLOCK, ROOT_ID, bDepthComm );
     }
-   
+
     //-----------------------------------------------------
 
     const double start = MPI_Wtime();
 
     matrix< MATRIX_TYPE >* resBlock = matrix_helper< MATRIX_TYPE >::mul( aBlock, bBlock );
-    //MPI_Datatype RESBLOCK = MPI_Type_vector_wrapper( resBlock->height(), resBlock->width(), resBlock->dataWidth(), MPI_TYPE );
+    MPI_Datatype RESBLOCK = MPI_Type_vector_wrapper( resBlock->height(), resBlock->width(), resBlock->dataWidth(), MPI_TYPE );
 
-    //matrix< MATRIX_TYPE > tempBlock( resBlock->height(), resBlock->width() );
-    //if ( myCDepthId == 0 )
-    //{
-    //    MPI_Status status;
-    //    for ( int i = 0; i < yDim; ++i )
-    //    {
-    //        checkres( MPI_Recv( tempBlock.raw(), 1, RESBLOCK, MPI_ANY_SOURCE, MPI_ANY_TAG, cDepthComm, &status ) );
-    //        resBlock->add( tempBlock );
-    //    }
-    //}
+    matrix< MATRIX_TYPE > tempBlock( resBlock->height(), resBlock->width() );
+    if ( myCDepthId == ROOT_ID )
+    {
+        MPI_Status status;
+        for ( int i = 1; i < yDim; ++i )
+        {
+            checkres( MPI_Recv( tempBlock.raw(), 1, RESBLOCK, MPI_ANY_SOURCE, MPI_ANY_TAG, cDepthComm, &status ) );
+            resBlock->add( tempBlock );
+        }
+    }
+    else
+    {
+        checkres( MPI_Send( resBlock->raw(), 1, RESBLOCK, ROOT_ID, 0, cDepthComm ) );
+    }
 
-    matrix_helper< MATRIX_TYPE >::print( *resBlock, std::cout );
+    //if ( myID == 0 )
+    if ( cubeCoords[1] == 0 )
+        matrix_helper< MATRIX_TYPE >::print( *resBlock, std::cout );
 
     delete resBlock;
     resBlock = 0;
