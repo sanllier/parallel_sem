3d4c2216634a03a61b5915a6318cb39c3eb0b969
Alex Gor <ifresh.wp@gmail.com>
2014-10-16 14:19:47 +0400

    task7_6: done

--- tasks/task7_6/main.cpp
+++ tasks/task7_6/main.cpp
@@ -49,7 +49,7 @@ int main( int argc, char** argv )
         throw "Invalid processor cube dims";
 
     //----------------------------------------------------
-    // HOLY CRAP
+
     const int dims[3] = { xDim, yDim, zDim  };
     const int periods[3] = { 0, 0, 0 };
     MPI_Comm cubeComm;
@@ -67,76 +67,66 @@ int main( int argc, char** argv )
     MPI_Comm cDepthComm;
     checkres( MPI_Cart_sub( cubeComm, remainDims, &cDepthComm ) );
 
+    remainDims[0] = 1; remainDims[1] = 1; remainDims[2] = 0;
+    MPI_Comm aSideComm;
+    checkres( MPI_Cart_sub( cubeComm, remainDims, &aSideComm ) );
+
+    remainDims[0] = 0; remainDims[1] = 1; remainDims[2] = 1;
+    MPI_Comm bSideComm;
+    checkres( MPI_Cart_sub( cubeComm, remainDims, &bSideComm ) );
+
+    remainDims[0] = 1; remainDims[1] = 0; remainDims[2] = 1;
+    MPI_Comm cSideComm;
+    checkres( MPI_Cart_sub( cubeComm, remainDims, &cSideComm ) );
+
+    int myADepthId = 0;
+    checkres( MPI_Comm_rank( aDepthComm, &myADepthId ) );
+
     int myCDepthId = 0;
     checkres( MPI_Comm_rank( cDepthComm, &myCDepthId ) );
 
     int myCubeRank = 0;
     checkres( MPI_Comm_rank( cubeComm, &myCubeRank ) );
 
+    int myASideRank = 0;
+    checkres( MPI_Comm_rank( aSideComm, &myASideRank ) );
+
+    int myBSideRank = 0;
+    checkres( MPI_Comm_rank( bSideComm, &myBSideRank ) );
+
+    int myCSideRank = 0;
+    checkres( MPI_Comm_rank( cSideComm, &myCSideRank ) );
+
     int cubeCoords[3];
     checkres( MPI_Cart_coords( cubeComm, myCubeRank, 3, cubeCoords ) );
 
+    int aSideCoords[2];
+    checkres( MPI_Cart_coords( aSideComm, myASideRank, 2, aSideCoords ) );
+
+    int bSideCoords[2];
+    checkres( MPI_Cart_coords( bSideComm, myBSideRank, 2, bSideCoords ) );
+
+    int cSideCoords[2];
+    checkres( MPI_Cart_coords( cSideComm, myCSideRank, 2, cSideCoords ) );
+
     //----------------------------------------------------
-   
+  
+    SHeader aHeader;
+    SHeader bHeader;
     matrix< MATRIX_TYPE > aBlock;
     matrix< MATRIX_TYPE > bBlock;
+    aBlock = parallelRead< MATRIX_TYPE, MPI_TYPE >( matAFile, myASideRank, xDim, yDim, aSideCoords, aSideComm, &aHeader );
+    bBlock = parallelRead< MATRIX_TYPE, MPI_TYPE >( matBFile, myBSideRank, yDim, zDim, bSideCoords, bSideComm, &bHeader );
 
-    MPI_Datatype ABLOCK;
-    MPI_Datatype BBLOCK;
+    if ( cubeCoords[2] != 0 )
+        aBlock = matrix< MATRIX_TYPE >( aHeader.height / xDim, aHeader.width / yDim );
+    if ( cubeCoords[0] != 0 )
+        bBlock = matrix< MATRIX_TYPE >( bHeader.height / yDim, bHeader.width / zDim );
 
-    if ( cubeCoords[2] == 0 )
-    {
-        matrix< MATRIX_TYPE > tempLine;
-
-        SHeader aSrcMatHeader;
-        SHeader aLineHeader;
-        tempLine = deserializeLine< MATRIX_TYPE >( matAFile, cubeCoords[0], xDim, &aSrcMatHeader, &aLineHeader );
-        long aBlockH = aSrcMatHeader.height / xDim;
-        long aBlockW = aSrcMatHeader.width / yDim;
-        aBlock.strongSubmatrix( tempLine, 0, aBlockW * cubeCoords[1], aBlockH, aBlockW );
-
-        long matrixesDims[2] = { aBlock.height(), aBlock.width() };
-        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
-        ABLOCK = MPI_Type_vector_wrapper( aBlock.height(), aBlock.width(), aBlock.dataWidth(), MPI_TYPE );
-        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
-    }
-    else
-    {
-        long matrixesDims[2];
-        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
-
-        ABLOCK = MPI_Type_vector_wrapper( matrixesDims[0], matrixesDims[1], matrixesDims[1], MPI_TYPE );
-
-        aBlock = matrix< MATRIX_TYPE >( matrixesDims[0], matrixesDims[1] );
-        MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm );
-    }
-    
-    if ( cubeCoords[0] == 0 )
-    {
-        matrix< MATRIX_TYPE > tempLine;
-
-        SHeader bSrcMatHeader;
-        SHeader bLineHeader;
-        tempLine = deserializeLine< MATRIX_TYPE >( matBFile, cubeCoords[1], zDim, &bSrcMatHeader, &bLineHeader );
-        long bBlockH = bSrcMatHeader.height / zDim;
-        long bBlockW = bSrcMatHeader.width / yDim;
-        bBlock.strongSubmatrix( tempLine, 0, bBlockW * cubeCoords[2], bBlockH, bBlockW );
-
-        long matrixesDims[2] = { bBlock.height(), bBlock.width() };
-        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
-        BBLOCK = MPI_Type_vector_wrapper( bBlock.height(), bBlock.width(), bBlock.dataWidth(), MPI_TYPE );
-        MPI_Bcast( bBlock.raw(), 1, BBLOCK, ROOT_ID, bDepthComm );
-    }
-    else
-    {
-        long matrixesDims[2];
-        MPI_Bcast( matrixesDims, 2, MPI_LONG, ROOT_ID, MPI_COMM_WORLD );
-
-        BBLOCK = MPI_Type_vector_wrapper( matrixesDims[0], matrixesDims[1], matrixesDims[1], MPI_TYPE );
-
-        bBlock = matrix< MATRIX_TYPE >( matrixesDims[0], matrixesDims[1] );
-        MPI_Bcast( bBlock.raw(), 1, BBLOCK, ROOT_ID, bDepthComm );
-    }
+    MPI_Datatype ABLOCK = MPI_Type_vector_wrapper( aBlock.height(), aBlock.width(), aBlock.dataWidth(), MPI_TYPE );
+    MPI_Datatype BBLOCK = MPI_Type_vector_wrapper( bBlock.height(), bBlock.width(), bBlock.dataWidth(), MPI_TYPE );
+    checkres( MPI_Bcast( aBlock.raw(), 1, ABLOCK, ROOT_ID, aDepthComm ) );
+    checkres( MPI_Bcast( bBlock.raw(), 1, BBLOCK, ROOT_ID, bDepthComm ) );
 
     //-----------------------------------------------------
 
@@ -163,77 +153,11 @@ int main( int argc, char** argv )
     const double end = MPI_Wtime();
     MASTERPRINT( "TOTAL TIME: " << end - start << "\r\n" );
 
-    //-----------------------------------------------------
-
-    if ( cubeCoords[1] == 0 )
-        matrix_helper< MATRIX_TYPE >::print( *resBlock, std::cout );
-
-    if ( cubeCoords[1] == 0 && cubeCoords[2] == 0 )
-    {
-        matrix< MATRIX_TYPE > resLine( resBlock->height(), resBlock->width() * zDim );
-        resLine.insertSubmatrix( *resBlock, 0, 0 );
-
-        MPI_Status status;
-        for ( int i = 1; i < zDim; ++i )
-        {
-            checkres( MPI_Recv( resBlock->raw(), 1, RESBLOCK, MPI_ANY_SOURCE, MPI_ANY_TAG, cubeComm, &status ) );
-            int coords[3];
-            MPI_Cart_coords( cubeComm, status.MPI_SOURCE, 3, coords );
-            resLine.insertSubmatrix( *resBlock, 0, coords[1] * resBlock->width() );
-        }
-
-        SHeader fullResHeader = { resLine.height() * xDim, resLine.width(), resLine.dataType(), resLine.matrixType() };
-        SHeader lineResHeader = { resLine.height(), resLine.width(), resLine.dataType(), resLine.matrixType() };
-
-        if ( cubeCoords[0] == 0 && cubeCoords[1] == 0 && cubeCoords[2] == 0 )
-        {
-            std::ofstream res( resFile, std::fstream::out | std::ios::binary );
-            if ( !res.good() )
-                return false;
-
-            res.write( (const char*)&fullResHeader, sizeof( Matrix::SHeader ) );
-            const char* raw = (char*)resLine.raw();
-            res.write( raw, ELEMENT_SIZE_BY_TYPE[ lineResHeader.dataType ] * resLine.height() * resLine.width() );
-            res.close();
-        }
-
-        MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-
-        for ( int i = 0; i < xDim; ++i )
-        {
-            if ( cubeCoords[0] == i && cubeCoords[1] == 0 && cubeCoords[2] == 0 )
-            {
-                std::fstream res( resFile, std::fstream::out | std::fstream::app | std::ios::binary );
-                if ( !res.good() )
-                    return false;
-
-                const char* raw = (char*)resLine.raw();
-                res.write( raw, ELEMENT_SIZE_BY_TYPE[ lineResHeader.dataType ] * resLine.height() * resLine.width() );
-                res.close();
-            }
-            MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-        }
-    }
-    else if ( cubeCoords[1] == 0 && cubeCoords[2] > 0 )
-    {
-        int coords[3] = { cubeCoords[0], 0, 0 };
-        int target;
-        MPI_Comm_rank( cubeComm, &target );
-        checkres( MPI_Send( resBlock->raw(), 1, RESBLOCK, target, 0, cubeComm ) );
-        MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-        for ( int i = 0; i < xDim; ++i )
-            MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-    }
-    else
-    {
-        MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-        for ( int i = 0; i < xDim; ++i )
-            MPI_Barrier( MPI_COMM_WORLD ); // CRAP
-    }
+    parallelWrite< MATRIX_TYPE, MPI_TYPE >( resFile, myCSideRank, xDim, zDim, cSideCoords, cSideComm, *resBlock );
 
     delete resBlock;
     resBlock = 0;
-
+    
     //-----------------------------------------------------
 
     checkres( MPI_Finalize() );
